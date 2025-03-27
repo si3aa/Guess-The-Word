e1f5dece94e8ad795d5125553e27b6aca1b5180f
si3aa <mahmoud22said22@gmail.com>
2025-03-27 18:51:06 +0200

    create game area and style key colors

---
+++ main.js
@@ -0,0 +1,5 @@
+// Setting Game Name
+const gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Game Created by Mahmoud Said`;
